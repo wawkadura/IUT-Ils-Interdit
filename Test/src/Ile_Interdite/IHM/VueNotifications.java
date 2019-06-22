5fe900f372e2795dc3dae6fddb52a24432828698
watrinc <watrinc@pc-dg-039-04.iut2.upmf-grenoble.fr>
2019-06-23 00:03:17 +0200

    Merge origin/master
    Conflicts:
    	Test/src/Ile_Interdite/IHM/VueAventurier.java

--- Test/src/Ile_Interdite/IHM/VueNotifications.java
+++ Test/src/Ile_Interdite/IHM/VueNotifications.java
@@ -23,10 +23,10 @@ public class VueNotifications extends Observe {
     private JFrame fenetre1;
     private JFrame fenetre2;
     private JFrame fenetre3;
-    
-    public void mainPlein(){
+
+    public void mainPlein() {
         fenetre1 = new JFrame("Attention");
-        JPanel haut = new JPanel(new GridLayout(3,1));
+        JPanel haut = new JPanel(new GridLayout(3, 1));
         JPanel bas = new JPanel();
         JLabel info1 = new JLabel("                            ATTENTION !");
         JLabel info2 = new JLabel("     Vous avez plus de 5 cartes en main ! ");
@@ -39,22 +39,21 @@ public class VueNotifications extends Observe {
             @Override
             public void actionPerformed(ActionEvent arg0) {
                 Message m = new Message();
-                m.type=TypesMessages.DEFAUSSER;
+                m.type = TypesMessages.DEFAUSSER;
                 notifierObservateur(m);
                 fenetre1.setVisible(false);
-                
-                
+
             }
         });
         bas.add(ok);
-        fenetre1.add(haut,BorderLayout.CENTER);
-        fenetre1.add(bas,BorderLayout.SOUTH);
+        fenetre1.add(haut, BorderLayout.CENTER);
+        fenetre1.add(bas, BorderLayout.SOUTH);
         afficher1();
     }
-    
-    public void vueGagner(){
+
+    public void vueGagner() {
         fenetre2 = new JFrame("Partie gagnée");
-        JPanel haut = new JPanel(new GridLayout(3,1));
+        JPanel haut = new JPanel(new GridLayout(3, 1));
         JPanel bas = new JPanel();
         JLabel info1 = new JLabel("                       FÉLICITATION !");
         JLabel info2 = new JLabel("     Vous venez de remporter la partie ! ");
@@ -64,26 +63,32 @@ public class VueNotifications extends Observe {
         quitter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                
+                Message m = new Message();
+                m.type = TypesMessages.QUITTER;
+                notifierObservateur(m);
+                fenetre2.setVisible(false);
             }
         });
         JButton recommencer = new JButton("RECOMMENCER");
         recommencer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                
+                Message m = new Message();
+                m.type = TypesMessages.RECOMMENCER;
+                notifierObservateur(m);
+                fenetre2.setVisible(false);
             }
         });
         bas.add(quitter);
         bas.add(recommencer);
-        fenetre2.add(haut,BorderLayout.CENTER);
-        fenetre2.add(bas,BorderLayout.SOUTH);
+        fenetre2.add(haut, BorderLayout.CENTER);
+        fenetre2.add(bas, BorderLayout.SOUTH);
         afficher2();
     }
-    
-    public void vuePerdu(){
+
+    public void vuePerdu() {
         fenetre3 = new JFrame("Partie perdu");
-        JPanel haut = new JPanel(new GridLayout(3,1));
+        JPanel haut = new JPanel(new GridLayout(3, 1));
         JPanel bas = new JPanel();
         JLabel info1 = new JLabel("                       DOMMAGE !");
         JLabel info2 = new JLabel("         Vous avez perdu la partie ! ");
@@ -93,39 +98,43 @@ public class VueNotifications extends Observe {
         quitter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                
+                Message m = new Message();
+                m.type = TypesMessages.QUITTER;
+                notifierObservateur(m);
+                fenetre3.setVisible(false);
             }
         });
         JButton recommencer = new JButton("RECOMMENCER");
         recommencer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                
+                Message m = new Message();
+                m.type = TypesMessages.RECOMMENCER;
+                notifierObservateur(m);
+                fenetre3.setVisible(false);
             }
         });
         bas.add(quitter);
         bas.add(recommencer);
-        fenetre3.add(haut,BorderLayout.CENTER);
-        fenetre3.add(bas,BorderLayout.SOUTH);
+        fenetre3.add(haut, BorderLayout.CENTER);
+        fenetre3.add(bas, BorderLayout.SOUTH);
         afficher3();
     }
-    
-    
-    
+
     public void afficher1() {
         //permet d'afficher la fenetre du jeu
         fenetre1.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         fenetre1.setSize(300, 200);
         fenetre1.setVisible(true);
     }
-    
+
     public void afficher2() {
         //permet d'afficher la fenetre du jeu
         fenetre2.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         fenetre2.setSize(300, 200);
         fenetre2.setVisible(true);
     }
-    
+
     public void afficher3() {
         //permet d'afficher la fenetre du jeu
         fenetre3.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
